6f762ae867d76ddbef2ff3e01faa8e456dd0dded
Nick Hall <nickhall122@gmail.com>
2017-10-31 09:00:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges (element) {
+  for (let i = 0; i <   element.length; i++){
+    console.log(`Welcome ${element[i]}! You are employee #${i+1}.`);
+  }
+  return element;
+}
+
+function tailsNeverFails () {
+  let tails = 0;
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
